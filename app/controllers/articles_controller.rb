6ddb9177de784f24a6c1237f4ebce92d5bd298ba
Yubin Jamora <yubinlee0726@gmail.com>
2019-05-23 13:44:37 -0400

    Added destroy feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,10 +23,6 @@ class ArticlesController < ApplicationController
     end
   end
 
-   def show
-     @article = Article.find(params[:id])
-   end
-
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
@@ -35,6 +31,17 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+   def show
+     @article = Article.find(params[:id])
+   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to article_path(@article)
   end
 
   private
